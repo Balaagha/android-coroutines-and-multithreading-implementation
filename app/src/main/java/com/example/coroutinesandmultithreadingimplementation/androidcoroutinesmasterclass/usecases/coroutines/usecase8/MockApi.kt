67729e1b3e8b8f0f633671497f90ffe203ffbe0e
Balaagha Alihumatov <balaagha.alihumatov@ibar.az>
2022-01-23 17:48:34 +0400

    add use case 8  ( using room with coroutines)

---
+++ app/src/main/java/com/example/coroutinesandmultithreadingimplementation/androidcoroutinesmasterclass/usecases/coroutines/usecase8/MockApi.kt
@@ -0,0 +1,17 @@
+package com.example.coroutinesandmultithreadingimplementation.androidcoroutinesmasterclass.usecases.coroutines.usecase8
+
+import com.example.coroutinesandmultithreadingimplementation.androidcoroutinesmasterclass.mock.createMockApi
+import com.example.coroutinesandmultithreadingimplementation.androidcoroutinesmasterclass.mock.mockAndroidVersions
+import com.example.coroutinesandmultithreadingimplementation.androidcoroutinesmasterclass.utils.MockNetworkInterceptor
+import com.google.gson.Gson
+
+fun mockApi() =
+    createMockApi(
+        MockNetworkInterceptor()
+            .mock(
+                "http://localhost/recent-android-versions",
+                Gson().toJson(mockAndroidVersions),
+                200,
+                5000
+            )
+    )
